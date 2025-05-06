b39f3b4d47f57f0d6c9ec8de8368f125e812e795
Nicholas <nicholastc14@gmail.com>
2025-05-05 21:24:03 -0700

    testing branch: should be wrong

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,11 +1,13 @@
 // sum.test.js
 import { sum } from '../code-to-unit-test/sum';
 
+// NOTE: FOR THE LAST PART OF THE ASSIGNMENT, CHANGED PARAMETER FROM 3 TO 4
+
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(3);
+  expect(1 + 2).toBe(4);
 });
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3);
+  expect(sum(1,2)).toBe(4);
 });
