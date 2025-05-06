1e57f11bf39e06792e54ed9c3feddcb80a901305
Nicholas <nicholastc14@gmail.com>
2025-05-05 21:36:01 -0700

    corrected, reverted back parameters to 3

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,13 +1,14 @@
 // sum.test.js
 import { sum } from '../code-to-unit-test/sum';
 
-// NOTE: FOR THE LAST PART OF THE ASSIGNMENT, CHANGED PARAMETER FROM 3 TO 4
+// NOTE 1: FOR THE LAST PART OF THE ASSIGNMENT, CHANGED PARAMETER FROM 3 TO 4
+// Note 2: reverted back to 3
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(4);
+  expect(sum(1,2)).toBe(3);
 });
